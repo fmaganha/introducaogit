732b9d90faf189b2c5bb0af863921e830b24f009
Filipe <maganha@outlook.com>
2021-03-08 20:43:10 -0300

    teste

--- Empregado.java
+++ Empregado.java
@@ -5,6 +5,33 @@ public class Empregado{
     private double salario;
     private double comissao;
     private double bonus;
+
+    public void setTipo (int tipo){
+        this.tipo = tipo;
+        }
+        public int tipo (){
+        return this.tipo;
+        }
+
+    public void setSalario (double salario){
+        this.salario = salario;
+        }
+        public double getSalario (){
+        return this.salario;
+        }
+        public void setComissao (double comissao){
+        this.comissao = comissao;
+        }
+        public double getComissao (){
+        return this.comissao;
+        }
+        public void setBonus (double bonus){
+        this.bonus = bonus;
+        }
+        public double getBonus (){
+        return this.bonus;
+        }
+
     public double calculaSalario (){
     if (tipo == 1){
     return salario;
